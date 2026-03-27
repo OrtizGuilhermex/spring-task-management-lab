6842397f0ef5d572a1d95fed68250c9ca641435f
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-03-27 16:31:29 -0300

    feat: adiciona estrutura inicial projeto

---
+++ src/main/java/com/ctw/spring_task_management_lab/SpringTaskManagementLabApplication.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_task_management_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskManagementLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskManagementLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctw/spring_task_management_lab/SpringTaskManagementLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_task_management_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskManagementLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
